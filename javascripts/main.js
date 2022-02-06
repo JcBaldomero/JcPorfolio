d88b51986fbc82018130be67a37f3d34ddae0f5f
Xcali <82700373+XcaliGit@users.noreply.github.com>
2022-02-06 18:26:51 +0000

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,91 @@
+  // navbar icon toggle
+  function toggleBtn() {
+      let nav = document.querySelector(".navlink-container");
+      nav.classList.toggle("open");
+  }
+
+
+//gives alert when user dl C
+  function downloadStart(){
+    alert('Download Curriculum vitae?');
+  }
+
+
+
+
+
+//gallery function
+
+const panels = document.querySelectorAll('.panel');
+
+
+
+function toggleActive() {
+  this.classList.toggle('open');
+} 
+
+function toggleShow(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('color')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+
+panels.forEach(panel => panel.addEventListener('click', toggleActive));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleShow));
+
+
+
+
+
+
+
+
+  
+
+// Scroll animation 
+
+          
+            // Detect request animation frame
+            var scroll = window.requestAnimationFrame ||
+            // IE Fallback
+            function(callback){ window.setTimeout(callback, 1000/60)};
+            var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+            
+            function loop() {
+            
+            Array.prototype.forEach.call(elementsToShow, function(element){
+            if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+            } else {
+            element.classList.remove('is-visible');
+            }
+            });
+            
+            scroll(loop);
+            }
+            
+            // Call the loop for the first time
+            loop();
+            
+            // Helper function from: http://stackoverflow.com/a/7557433/274826
+            function isElementInViewport(el) {
+            // special bonus for those using jQuery
+            if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+            }
+            var rect = el.getBoundingClientRect();
+            return (
+            (rect.top <= 0
+            && rect.bottom >= 0)
+            ||
+            (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+            ||
+            (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+            );
+            }
+            
+            
